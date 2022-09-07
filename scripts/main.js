ce5ef259e303e121201790a64fa3a1437f2c6ccb
Davina Leong <davina.leong.sy@gmail.com>
2022-09-07 15:42:04 +0800

    Added accordion functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,36 @@
 console.log(`main.js loaded`)
+
+const accordionPaneEls = document.querySelectorAll(
+  `[data-accordion] .accordion-pane`
+)
+const attributeOpen = `data-open`
+
+const collapseAllPanes = () => {
+  accordionPaneEls.forEach((el) => el.removeAttribute(attributeOpen))
+}
+
+const accordionToggleEls = document.querySelectorAll(
+  `[data-accordion] .accordion-toggle`
+)
+const accordionTitleEls = document.querySelectorAll(
+  `[data-accordion] .accordion-title`
+)
+const accordionArrowEls = document.querySelectorAll(
+  `[data-accordion] .accordion-arrow`
+)
+
+collapseAllPanes()
+
+accordionTitleEls.forEach((el) => {
+  el.addEventListener(`click`, (e) => {
+    collapseAllPanes()
+    e.target.parentElement.parentElement.toggleAttribute(attributeOpen)
+  })
+})
+
+accordionArrowEls.forEach((el) => {
+  el.addEventListener(`click`, (e) => {
+    collapseAllPanes()
+    e.target.parentElement.parentElement.toggleAttribute(attributeOpen)
+  })
+})
